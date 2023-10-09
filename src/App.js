90aa15f6454763227b05256627955c795c4ddf9f
nadiatkachuk <126887250+nadiatkachuk@users.noreply.github.com>
2023-10-09 04:57:20 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Nadiia Tkachuk {""}
-        <a href="https://github.com/nadiatkachuk/react-weather-app">
-          open sourced on Github
-        </a>
-      </footer>
+      <div class="container">
+        <h1 class="text-success">Weather App</h1>
+
+        <footer>
+          This project was coded by Nadiia Tkachuk {""}
+          <a href="https://github.com/nadiatkachuk/react-weather-app">
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
